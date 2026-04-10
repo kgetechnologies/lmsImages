49729f95f2c9ead0e94cdf5f80d66a898dbcedea
Sarika Malhotra <malhotrasarika789@gmail.com>
2026-04-10 12:27:00 +0530

    Update CustomPublic.js

--- css/CustomPublic.js
+++ css/CustomPublic.js
@@ -1,6 +1,25 @@
-console.log("JS FILE LOAD HO GAYI HAI!");
-alert("System Working!");
+document.addEventListener('DOMContentLoaded', function() {
+    const dateInput = document.querySelector('.bookingrange') || document.querySelector('input[placeholder*="dd/mm/yyyy"]');
 
-document.onclick = function(e) { 
-    console.log("Aapne click kiya:", e.target); 
-};
+    if (dateInput) {
+        
+        dateInput.style.cursor = "pointer";
+
+        
+        dateInput.addEventListener('click', function() {
+            this.type = 'date';
+            
+            
+            if (this.showPicker) {
+                this.showPicker();
+            }
+        });
+
+        
+        dateInput.addEventListener('blur', function() {
+            if (!this.value) {
+                this.type = 'text';
+            }
+        });
+    }
+});
