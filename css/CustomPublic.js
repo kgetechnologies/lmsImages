5b3571dedfc87c4822c115f00af80b4c1dddd6f3
Sarika Malhotra <malhotrasarika789@gmail.com>
2026-04-10 20:28:14 +0530

    Update CustomPublic.js

--- css/CustomPublic.js
+++ css/CustomPublic.js
@@ -60,3 +60,13 @@ function fixCalendarBug() {
 
 window.onload = fixCalendarBug;
 setInterval(fixCalendarBug, 1500);
+
+
+
+
+window.addEventListener('load', function() {
+    document.querySelectorAll('.main-nav li a').forEach(item => {
+        item.style.setProperty('color', '#000000', 'important');
+        item.style.setProperty('font-weight', 'bold', 'important');
+    });
+});
